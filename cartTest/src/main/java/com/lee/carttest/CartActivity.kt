22bf789180bfa36b246ec0915b10ec220111b9e3
lndlazy <lndlazy@163.com>
2023-02-16 13:46:47 +0800

    CartActivity.kt 添加路由

--- cartTest/src/main/java/com/lee/carttest/CartActivity.kt
+++ cartTest/src/main/java/com/lee/carttest/CartActivity.kt
@@ -1,12 +1,20 @@
 package com.lee.carttest
 
+import com.alibaba.android.arouter.facade.annotation.Route
 import com.lee.commontest.base.BaseActivity
 
+@Route(path = "cart/cartActivity")
 class CartActivity : BaseActivity() {
 
     override fun getViewId(): Int = R.layout.activity_cart
 
     override fun initData() {
+
+        val stringExtra = intent.getStringExtra("str")
+        val intExtra = intent.getIntExtra("int", -1)
+        val booleanExtra = intent.getBooleanExtra("boolean", false)
+        println("str:$stringExtra, intExtra:${intExtra}, booleanExtra:$booleanExtra")
+
     }
 
     override fun initView() {
