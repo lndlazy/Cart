83ee806b2cc974f509f89180f5d80e2940a3f907
lndlazy <lndlazy@163.com>
2023-02-16 13:58:17 +0800

    路由数据接受

--- cartTest/src/main/java/com/lee/carttest/CartActivity.kt
+++ cartTest/src/main/java/com/lee/carttest/CartActivity.kt
@@ -1,6 +1,8 @@
 package com.lee.carttest
 
+import android.widget.Button
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
 import com.lee.commontest.base.BaseActivity
 
 @Route(path = "cart/cartActivity")
@@ -10,14 +12,23 @@ class CartActivity : BaseActivity() {
 
     override fun initData() {
 
-        val stringExtra = intent.getStringExtra("str")
-        val intExtra = intent.getIntExtra("int", -1)
-        val booleanExtra = intent.getBooleanExtra("boolean", false)
+        val stringExtra = intent?.getStringExtra("str")
+        val intExtra = intent?.getIntExtra("int", -1)
+        val booleanExtra = intent?.getBooleanExtra("boolean", false)
         println("CartActivity ==>>> str:$stringExtra, intExtra:${intExtra}, booleanExtra:$booleanExtra")
 
     }
 
     override fun initView() {
+
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            //cart跳转到首页
+            ARouter.getInstance().build("home/homeActivity")
+                .withString("str", "cartActivity跳转过来的")
+                .withInt("int", 555)
+                .withBoolean("boolean", true)
+                .navigation()
+        }
 
     }
 }
