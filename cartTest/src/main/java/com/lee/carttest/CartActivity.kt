c42f0d6d76bc45b2febe246c0cbbcade7b372b14
lndlazy <lndlazy@163.com>
2023-02-16 11:50:37 +0800

    cart first commit

---
+++ cartTest/src/main/java/com/lee/carttest/CartActivity.kt
@@ -0,0 +1,15 @@
+package com.lee.carttest
+
+import com.lee.commontest.base.BaseActivity
+
+class CartActivity : BaseActivity() {
+
+    override fun getViewId(): Int = R.layout.activity_cart
+
+    override fun initData() {
+    }
+
+    override fun initView() {
+
+    }
+}
